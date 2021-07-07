6ea0a2fc865a423cf42d0ce9797ba55604073885
J4yy <spyzzs11@gmail.com>
2021-07-08 02:21:30 +0700

    add entity FRCF

---
+++ ShopElectronic.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace ShopElectronic.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private ShopElecttronicDbContext     dbContext;
+
+        public ShopElecttronicDbContext Init()
+        {
+            return dbContext ?? (dbContext = new ShopElecttronicDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
